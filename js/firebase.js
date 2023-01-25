857354c0947eac92f012f94106f30be45537a82e
Alrik He <alrik.he@gritacademy.se>
2023-01-25 16:06:03 +0100

    DB implementation and cards , my db replace later ty

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 //import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
 import {
-  getDatabase,
-  ref,
-  set,
-  onValue,
-  remove,
-  push,
+    getDatabase,
+    ref,
+    set,
+    onValue,
+    remove,
+    push,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,74 +14,78 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = { // change all credentials to make it work
-  databaseURL:
-    "https://onlinedatabase-2cd29-default-rtdb.europe-west1.firebasedatabase.app",
-  apiKey: "AIzaSyBuwc5HIp8-VaojCoNH904rph-YQwv9ugc",
-  authDomain: "onlinedatabase-2cd29.firebaseapp.com",
-  projectId: "onlinedatabase-2cd29",
-  storageBucket: "onlinedatabase-2cd29.appspot.com",
-  messagingSenderId: "513260891542",
-  appId: "1:513260891542:web:a3af24974e5dbd521381b7",
-  measurementId: "G-1QXQ3M2ZE2",
+const firebaseConfig = {
+    databaseURL:
+        "https://demo1-3c759-default-rtdb.europe-west1.firebasedatabase.app/", // <----
+    apiKey: "AIzaSyDkRmoWLJ0eEXZKYEeUNwRF8V6X0oHwBi0",
+    authDomain: "demo1-3c759.firebaseapp.com",
+    projectId: "demo1-3c759",
+    storageBucket: "demo1-3c759.appspot.com",
+    messagingSenderId: "632318648569",
+    appId: "1:632318648569:web:60f9813437fa829e598228",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
-const database = getDatabase();
-
-// skriva
-function writeUserData(message) {
-  let adressRef = ref(database, "Eleonora/barn1");
-  set(adressRef, {
-    message: message,
-    username: "EL",
-    lista: [888, 2, 3, 4],
-  });
+const db = getDatabase();
+var urlRootRef = ref(db, "/");
+const MAX_IMAGES= 13
+let container = document.getElementsByClassName("comments-container")[0];
+function createCard(user, text) {
+    container.innerHTML += `
+<div class="card">
+<div class="card-img">
+    <div class="user-bild" style="background-image: url('${arrayWithPicURL[Math.floor(Math.random()*MAX_IMAGES)]}');"></div>
+</div>
+<div class="comments-text">
+    <h4>${user}</h4>
+    <p>${text}</p>
+</div>
+</div>
+`;
 }
 
-writeUserData("SUPP DOG!!!");
+let arrayWithPicURL = [];
+function getfiles() {
+    for (let index = 1; index <= MAX_IMAGES; index++) {
+        arrayWithPicURL.push("index.html/../images/profile/" + index.toString().padStart(3,"0")+".png");
+    }
+    console.log(arrayWithPicURL);
+}
+getfiles();
 
-// läsa en specifik onValue direkt + när den ändrar value
-const urlRef = ref(database, "Eleonora/barn1");
-onValue(urlRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data.lista);
-  document.body.innerHTML = data.lista[1];
-});
+/*var filename = url.substring(url.lastIndexOf('/')+1);
+var urlTOProfiles= ("index.html/../images/profile");
+-*/
 
-const urlRootRef = ref(database, "/Eleonora");
-//läsa childs i root.
-console.warn("LOOP and children!!");
-onValue(
-  urlRootRef,
-  (snapshot) => {
+//alert(filename);
+
+onValue(urlRootRef, (snapshot) => {
+    container.innerHTML = ""; // clear the container before looping and creating new cards
     snapshot.forEach((childSnapshot) => {
-      const childKey = childSnapshot.key;
-      const childData = childSnapshot.val();
-      console.log(childKey);
-      console.log(childData.message);
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        console.log(childKey);
+        console.log(childData.message);
+        createCard(childData.username, childData.message);
     });
-  } /* ,
-    {
-        onlyOnce: true,  // engång
-    } */
-);
-
-//radera stuff.
-const deleteRef = ref(database, "/filip/username");
-remove(deleteRef).then(() => {
-  console.log("location removed");
 });
 
-// kolla om det supportas i olika webb läsare
-if (typeof Storage !== "undefined") {
-  // läsa från user pref
-  console.log(localStorage.getItem("name"));
-  console.log(localStorage.getItem("lastLoggin"));
 
-  //skriva i user pref
-  //localStorage.setItem("name", "Alrik");
-  // localStorage.setItem("lastLoggin","today!!");
-}
+let input = document.getElementById("input")
+document.getElementsByClassName("upload-bottom")[0].addEventListener("click",(event)=>
+{
+
+        let adressRef = ref(db, "/");
+        push(adressRef, {
+            username: "username",
+            dateOfCretion: new Date().toString("yyyy-MM-dd hh:mm:ss"),
+            message: input.value,
+            x: 0,
+            y: 0,
+        });
+   
+input.value="" //clear text input
+});
+
