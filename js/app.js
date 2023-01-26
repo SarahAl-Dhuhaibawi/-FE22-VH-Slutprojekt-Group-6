110d0e1139f4889279d645ab980ed8b2649a59f3
MGritt <112488649+MGritt@users.noreply.github.com>
2023-01-26 10:53:16 +0100

    typewriter effect för post box (what's on your mind?)

--- js/app.js
+++ js/app.js
@@ -12,3 +12,30 @@ toggleBtn.addEventListener('click', function () {
 
     dropdownMenu.classList.toggle('show');
 })
+
+var input = document.getElementById('input')
+
+var customNodeCreator = function(character) {
+  // Add character to input placeholder
+  input.placeholder = input.placeholder + character;
+
+  // Return null to skip internal adding of dom node
+  return null;
+}
+
+var onRemoveNode = function({ character }) {
+  if(input.placeholder) {
+    // Remove last character from input placeholder
+    input.placeholder = input.placeholder.slice(0, -1)
+  }
+}
+
+var typewriter = new Typewriter(null, {
+  delay: 45,
+  onCreateTextNode: customNodeCreator,
+  onRemoveNode: onRemoveNode,
+});
+
+typewriter
+  .typeString("What's on your mind?")
+  .start();
